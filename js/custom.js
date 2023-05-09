3c6765cbd9247d6d06b1d51caab2ad2148ce0bc7
mkaar <markokaar@hotmail.com>
2023-05-09 00:37:56 -0700

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    // Banner Carousel
+    var myCarousel = document.querySelector('#myCarousel')
+    var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 5000,
+    })
+
+    // REVIEWS NAVIGATION
+    function ReviewsNavResize(){
+      $(".navbar").scrollspy({ offset: -94 });
+
+      var ReviewsOwlItem = $('.reviews-carousel .owl-item').width();
+
+      $('.reviews-carousel .owl-nav').css({'width' : (ReviewsOwlItem) + 'px'});
+    }
+
+    $(window).on("resize", ReviewsNavResize);
+    $(document).on("ready", ReviewsNavResize);
+
+    // HREF LINKS
+    $('a[href*="#"]').click(function (event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top - 74
+          }, 50);
+        }
+      }
+    });
+    
+  })(window.jQuery);
